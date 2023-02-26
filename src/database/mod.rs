92441a94ff6fe9e75e05700011dc706d074dab59
Artur Panach <1930175+arturictus@users.noreply.github.com>
2023-02-26 16:28:35 +0000

    renaming more

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -7,86 +7,86 @@ pub use query::RequestBuilder;
 pub use repo::ping_db;
 use std::time::Instant;
 
-pub fn get_sequences(q: DBRequest) -> Result<Vec<(std::string::String, u32)>, PgError> {
+pub fn get_sequences(r: DBRequest) -> Result<Vec<(std::string::String, u32)>, PgError> {
     duration::<Vec<(String, u32)>>(
-        format!("Getting sequences from {}", q.db.name()),
-        q,
+        format!("Getting sequences from {}", r.db.name()),
+        r,
         repo::get_sequences,
     )
 }
-pub fn get_greatest_id_from(q: DBRequest) -> Result<u32, PgError> {
-    let table = q.table.as_ref().unwrap();
+pub fn get_greatest_id_from(r: DBRequest) -> Result<u32, PgError> {
+    let table = r.table.as_ref().unwrap();
     duration::<u32>(
-        format!("Greatest id from `{table}` in {}", q.db.name()),
-        q,
+        format!("Greatest id from `{table}` in {}", r.db.name()),
+        r,
         repo::get_greatest_id_from,
     )
 }
 
-pub fn get_row_by_id_range(query: DBRequest) -> Result<Vec<String>, PgError> {
+pub fn get_row_by_id_range(r: DBRequest) -> Result<Vec<String>, PgError> {
     duration::<Vec<String>>(
         format!(
             "`{}` rows with ids from `{}` to `{}` in {}",
-            query.table.clone().unwrap(),
-            query.bounds.unwrap().0,
-            query.bounds.unwrap().1,
-            query.db.name()
+            r.table.clone().unwrap(),
+            r.bounds.unwrap().0,
+            r.bounds.unwrap().1,
+            r.db.name()
         ),
-        query,
+        r,
         repo::get_row_by_id_range,
     )
 }
-pub fn count_for(query: DBRequest) -> Result<u32, PgError> {
+pub fn count_for(r: DBRequest) -> Result<u32, PgError> {
     duration::<u32>(
         format!(
             "count from {} in {}",
-            query.table.as_ref().unwrap(),
-            query.db.name()
+            r.table.as_ref().unwrap(),
+            r.db.name()
         ),
-        query,
+        r,
         repo::count_for,
     )
 }
 
-pub fn all_tables(q: DBRequest) -> Result<Vec<String>, PgError> {
+pub fn all_tables(r: DBRequest) -> Result<Vec<String>, PgError> {
     duration::<Vec<String>>(
-        format!("Getting all tables for {}", q.db.name()),
-        q,
+        format!("Getting all tables for {}", r.db.name()),
+        r,
         repo::all_tables,
     )
 }
 
-pub fn tables_with_column(q: DBRequest) -> Result<Vec<String>, PgError> {
-    let column = q.column.as_ref().unwrap();
+pub fn tables_with_column(r: DBRequest) -> Result<Vec<String>, PgError> {
+    let column = r.column.as_ref().unwrap();
     duration::<Vec<String>>(
         format!(
             "Getting all tables with column {} in {}",
             column,
-            q.db.name()
+            r.db.name()
         ),
-        q,
+        r,
         repo::tables_with_column,
     )
 }
 
-pub fn id_and_column_value(q: DBRequest) -> Result<Vec<String>, PgError> {
+pub fn id_and_column_value(r: DBRequest) -> Result<Vec<String>, PgError> {
     duration::<Vec<String>>(
         format!(
             "Getting `id` and values from column `{}` from table {} in {}",
-            q.column.as_ref().unwrap(),
-            q.table.as_ref().unwrap(),
-            q.db.name()
+            r.column.as_ref().unwrap(),
+            r.table.as_ref().unwrap(),
+            r.db.name()
         ),
-        q,
+        r,
         repo::id_and_column_value,
     )
 }
 
-pub fn full_row_ordered_by(q: DBRequest) -> Result<Vec<String>, PgError> {
-    let table = q.table.as_ref().unwrap();
+pub fn full_row_ordered_by(r: DBRequest) -> Result<Vec<String>, PgError> {
+    let table = r.table.as_ref().unwrap();
     duration::<Vec<String>>(
-        format!("Getting rows from table {table} in {}", q.db.name()),
-        q,
+        format!("Getting rows from table {table} in {}", r.db.name()),
+        r,
         repo::full_row_ordered_by,
     )
 }
